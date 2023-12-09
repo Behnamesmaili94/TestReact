45e5855206014bbb36bc94df78525787f00852aa
NothingProgrammer <NothingProgrammer@DESKTOP-470ABRI>
2023-12-09 12:45:09 +0330

    show person add

--- src/App.js
+++ src/App.js
@@ -9,23 +9,31 @@ class App extends Component {
    }
 
    state={
-    
     person:[
         {id:0,name:"behnam",family:"esmaili",age:25},
         {id:1,name:"behzad",family:"esmaili",age:23},
         {id:2,name:"moslem",family:"sarhady",age:25},
-    ]
+    ],
+    showPersons:false,
   }
   
- 
+  deletPerson=()=>{
+    this.setState({showPersons:true})
+  }
+
   render() {
-    const {person} = this.state
-    
+    const {person ,id} = this.state
+    const personShow = this.state.showPersons
+
     return (
       <div>
-        
-        {<Home key={this.state.id} data={person}/>}
+
+        {<Home key={id} data={person}/>}
+        <div>
+          <button onClick={this.deletPerson}>delet persons</button>
+        </div>
       </div>
+      
     )
   }
 }
